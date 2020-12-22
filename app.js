eae499d1d531f551a49c8671f7b4622ecd5fcae8
gmpbigsun <Tuncay.Yildiz@Live.De>
2020-12-22 15:34:49 +0100

    Triggering Issues

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var express = require('express');
+var express = require("express");
 var app     = express();
-var server  = require('http').createServer(app);
-var io      = require('socket.io')(server);
+var server  = require("http").createServer(app);
+var io      = require("socket.io")(server);
 
 //Add recent chats to messages array
 var messages  = [];
@@ -14,38 +14,38 @@ var storeMessage = function(name, data){
 };
 
 //Setup the app with Express
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 //Socket.io
-io.on('connection', function(socket){
+io.on("connection", function(socket){
 
   //Log activity
-  socket.on('join', function(name){
+  socket.on("join", function(name){
     socket.userName = name;
-    socket.broadcast.emit('chat', name + ' has joined the chat');
-    console.log(name + ' has joined the chat');
+    socket.broadcast.emit("chat", name + " has joined the chat");
+    console.log(name + " has joined the chat");
 
     //Log who has left
-    socket.on('disconnect', function(){
-      socket.broadcast.emit('chat', name + ' has left the chat');
-      console.log(name + ' has left the chat');
+    socket.on("disconnect", function(){
+      socket.broadcast.emit("chat", name + " has left the chat");
+      console.log(name + " has left the chat");
     });
   });
 
   //Log chats
-  socket.on('chat', function(message){
-    io.emit('chat', socket.userName + ': ' + message);
+  socket.on("chat", function(message){
+    io.emit("chat", socket.userName + ": " + message);
     storeMessage(socket.userName, message);
-    console.log(socket.userName + ': ' + message);
+    console.log(socket.userName + ": " + message);
   });
 
   //Log previous chats for new users
   messages.forEach(function(message){
-    socket.emit('chat', message.name + ': ' + message.data);
+    socket.emit("chat", message.name + ": " + message.data);
   });
 });
 
 //Listen at localhost:3000
 server.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
